20bd641677fa0ebc05cc3986ce7f99200bb0034f
Kehinde Onadipe <kenodipe@gmail.com>
2018-08-26 23:16:50 -0500

    modify css for index.html

--- js/main.js
+++ js/main.js
@@ -157,7 +157,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
-
+  li.className = "col-1-3";   
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
